a225c98dc137fb0ae30f3eec4272d4f2e654ab76
Yang Cun <yangcun@live.com>
2015-01-29 21:40:54 +0800

    del un used class file: new task view controller.

---
+++ WorkAndRest/ViewControllers/NewTaskViewController.swift
@@ -0,0 +1,23 @@
+//
+//  NewTaskViewController.swift
+//  WorkAndRest
+//
+//  Created by YangCun on 15/1/29.
+//  Copyright (c) 2015年 YangCun. All rights reserved.
+//
+
+import UIKit
+
+class NewTaskViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
