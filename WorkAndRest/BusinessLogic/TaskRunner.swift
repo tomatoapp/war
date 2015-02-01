09168fa6877b9c4f0821dd47faaae8d8d26e8ffb
Yang Cun <yangcun@live.com>
2015-02-01 18:31:44 +0800

    add the task runner class.

---
+++ WorkAndRest/BusinessLogic/TaskRunner.swift
@@ -0,0 +1,13 @@
+//
+//  TaskRunner.swift
+//  WorkAndRest
+//
+//  Created by YangCun on 15/2/1.
+//  Copyright (c) 2015年 YangCun. All rights reserved.
+//
+
+import UIKit
+
+class TaskRunner: NSObject {
+   
+}
