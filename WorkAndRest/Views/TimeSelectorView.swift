cf28ecaa8783693455a1b1a57f71ed54dc4b5e32
Yang Cun <yangcun@live.com>
2015-02-03 15:12:54 +0800

    Set the new icon and the new name.

---
+++ WorkAndRest/Views/TimeSelectorView.swift
@@ -0,0 +1,21 @@
+//
+//  TimeSelectorView.swift
+//  WorkAndRest
+//
+//  Created by YangCun on 15/2/3.
+//  Copyright (c) 2015年 YangCun. All rights reserved.
+//
+
+import UIKit
+
+class TimeSelectorView: UIPickerView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
