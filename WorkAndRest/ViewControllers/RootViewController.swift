3fe3f0f996a339c62c355e35527351f4809e1168
Yang Cun <yangcun@live.com>
2015-02-05 15:32:09 +0800

    Switch tabs with an animation.

--- WorkAndRest/ViewControllers/RootViewController.swift
+++ WorkAndRest/ViewControllers/RootViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class RootViewController: UITabBarController {
+class RootViewController: UITabBarController, UITabBarControllerDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,13 +16,64 @@ class RootViewController: UITabBarController {
         
         self.navigationController?.navigationBar.setBackgroundImage(UIImage(named: "white"), forBarMetrics: UIBarMetrics.Default)
         self.navigationController?.navigationBar.translucent = false
+        
+        //self.tabBarController!.delegate = self
+        self.delegate = self
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(animated)
+        // self.animationSwitch()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
+    func animationSwitch() {
+        let fromView = self.selectedViewController!.view
+        let controllerIndex = find(self.viewControllers! as Array, self.selectedViewController!) == 0 ? 1 : 0
+        let toView = self.viewControllers![controllerIndex].view as UIView!
+        
+        let viewSize = fromView.frame
+        let scrollRight = controllerIndex > self.selectedIndex
+        
+        fromView.superview!.addSubview(toView!)
+        
+        let width = UIScreen.mainScreen().bounds.size.width
+        toView.frame = CGRectMake(scrollRight ? width : -width, viewSize.origin.y, width, viewSize.height)
+        UIView.animateWithDuration(0.3, animations: { () -> Void in
+            fromView.frame = CGRectMake(scrollRight ? -width : width, viewSize.origin.y, width, viewSize.height)
+        }) { (finished) -> Void in
+            if finished {
+                fromView.removeFromSuperview()
+                self.selectedIndex = controllerIndex
+            }
+        }
+    }
+    
+    // MARK: - UITabBarControllerDelegate
+    
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        let fromView = tabBarController.selectedViewController?.view
+        let toView = viewController.view
+        
+        if fromView == toView {
+            return false
+        }
+        let fromIndex = find(tabBarController.viewControllers! as Array, tabBarController.selectedViewController!)
+        let toIndex = find(tabBarController.viewControllers! as Array, viewController)
+        
+        UIView.transitionFromView(fromView!,
+            toView: toView,
+            duration: 0.2,
+            options: UIViewAnimationOptions.TransitionCrossDissolve)
+            { (finished) -> Void in
+                if finished {
+                    tabBarController.selectedIndex = toIndex!
+                }
+        }
+        return true
+    }
+    
 }
