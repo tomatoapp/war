0d90db506a32326633b2cef8b85e20e689317145
Yang Cun <yangcun@live.com>
2015-03-06 17:53:38 +0800

    Add the productsManager class.

---
+++ WorkAndRest/BusinessLogic/ProductsManager.swift
@@ -0,0 +1,144 @@
+//
+//  ProductsManager.swift
+//  WorkAndRest
+//
+//  Created by YangCun on 3/6/15.
+//  Copyright (c) 2015 YangCun. All rights reserved.
+//
+
+import UIKit
+
+protocol ProductsManagerDelegate {
+    func productsManager(productsManager: ProductsManager, paymentTransactionState state: SKPaymentTransactionState)
+}
+
+private let _singletonInstance = ProductsManager()
+class ProductsManager: NSObject, SKProductsRequestDelegate, SKPaymentTransactionObserver {
+    let kProProductIdentifier = "com.yangcun.WorkAndRest.pro"
+    var delegate: ProductsManagerDelegate?
+    
+    class var sharedInstance: ProductsManager {
+        return _singletonInstance
+    }
+    // button
+    func purchasePro() {
+        println("User requests to purchase Pro.")
+        
+        if SKPaymentQueue.canMakePayments() {
+            println("User can make payments.")
+            
+            let productsRequest = SKProductsRequest(productIdentifiers: NSSet(object: kProProductIdentifier))
+            productsRequest.delegate = self
+            productsRequest.start()
+        } else {
+            println("User cannot make payments due to  parental controls.")
+        }
+    }
+    
+    func purchase(product: SKProduct) {
+        let payment = SKPayment(product: product)
+        SKPaymentQueue.defaultQueue().addTransactionObserver(self)
+        SKPaymentQueue.defaultQueue().addPayment(payment)
+    }
+    
+    // button
+    func restore() {
+        println("TransactionState -> restore()")
+        SKPaymentQueue.defaultQueue().restoreCompletedTransactions()
+        SKPaymentQueue.defaultQueue().addTransactionObserver(self)
+    }
+    
+    // MARK: - SKProductsRequestDelegate
+    
+    func productsRequest(request: SKProductsRequest!, didReceiveResponse response: SKProductsResponse!) {
+        
+        for invalidProductId in response.invalidProductIdentifiers {
+            println("invalidProductId: \(invalidProductId)")
+        }
+        
+        var validProduct: SKProduct?
+        let count = response.products.count
+        println("count:\(count)")
+        if count > 0 {
+            println("Products availabel!")
+            validProduct = response.products[0] as? SKProduct
+        } else if validProduct == nil {
+            println("No products available.")
+        }
+    }
+    
+    // MARK: - SKPaymentTransactionObserver
+    
+    func paymentQueue(queue: SKPaymentQueue!, updatedTransactions transactions: [AnyObject]!) {
+        for transaction in transactions {
+            switch transaction.transactionState! {
+            case SKPaymentTransactionState.Purchasing:
+                println("TransactionState -> Purchasing")
+                // called when the user is in the process of purchasing, do not add any of your own code here.
+                break
+                
+            case SKPaymentTransactionState.Purchased:
+                println("TransactionState -> Purchased")
+                
+                // this is called when the user has successfully purchased the package (Cha-Ching!)
+                self.doRemoveAds()
+                SKPaymentQueue.defaultQueue().finishTransaction(transaction as SKPaymentTransaction)
+                break
+                
+            case SKPaymentTransactionState.Restored:
+                println("TransactionState -> Restored")
+                
+                self.doRemoveAds()
+                SKPaymentQueue.defaultQueue().finishTransaction(transaction as SKPaymentTransaction)
+                break
+                
+            case SKPaymentTransactionState.Failed:
+                println("TransactionState -> Failed")
+                
+                if transaction.errorStatusCode == SKErrorPaymentCancelled {
+                    // The user cancelled the payment
+                    println("TransactionState -> Cancelled")
+                    
+                }
+                SKPaymentQueue.defaultQueue().finishTransaction(transaction as SKPaymentTransaction)
+                break
+                
+            default:
+                break
+            }
+            //self.delegate?.productsManager(self, paymentTransactionState: transaction.transactionState)
+        }
+    }
+    
+    func paymentQueueRestoreCompletedTransactionsFinished(queue: SKPaymentQueue!) {
+        println("Received restored transactions: \(queue.transactions.count)")
+        for transaction in queue.transactions {
+            if transaction.transactionState == SKPaymentTransactionState.Restored {
+                println("TransactionState -> Restored")
+                self.doRemoveAds()
+                SKPaymentQueue.defaultQueue().finishTransaction(transaction as SKPaymentTransaction)
+                break
+            }
+        }
+    }
+    
+    func doRemoveAds() {
+        println("doRemoveAds")
+        ApplicationStateManager.sharedInstance.purchasedSuccess()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- WorkAndRest/ViewControllers/SettingsViewController.swift
+++ WorkAndRest/ViewControllers/SettingsViewController.swift
@@ -10,10 +10,11 @@ import UIKit
 import MessageUI
 
 let SubTitleSectionHeight: CGFloat = 50
-class SettingsViewController: BaseTableViewController, UIAlertViewDelegate, MFMailComposeViewControllerDelegate {
+class SettingsViewController: BaseTableViewController, UIAlertViewDelegate, MFMailComposeViewControllerDelegate, ProductsManagerDelegate {
 
     @IBOutlet var badgeAppIconSwitch: UISwitch!
-
+    @IBOutlet var currentVersionLabel: UILabel!
+    let versionType = ApplicationStateManager.sharedInstance.versionType()
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +22,11 @@ class SettingsViewController: BaseTableViewController, UIAlertViewDelegate, MFMa
         self.badgeAppIconSwitch.on = NSUserDefaults.standardUserDefaults().boolForKey(GlobalConstants.kBOOL_BADGEAPPICON)
     }
 
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        self.tableView.reloadData()
+        self.refreshThePaymentItem(self.versionType)
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
@@ -37,15 +43,23 @@ class SettingsViewController: BaseTableViewController, UIAlertViewDelegate, MFMa
         if section == 0 {
             return 20
         }
-        if section == 1 || section == 3 {
+        if section == 1 {
             return SubTitleSectionHeight
+        }
+
+        if section == 2 {
+            if versionType == .Free {
+                return SubTitleSectionHeight
+            } else {
+                return 15
+            }
         }
         return 15
     }
     
     override func tableView(tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
       
-        if section != 1 && section != 3 {
+        if section != 1 && section != 2 {
             return nil
         }
         
@@ -59,8 +73,10 @@ class SettingsViewController: BaseTableViewController, UIAlertViewDelegate, MFMa
         if section == 1 {
             label.text = "Show incomplete task count badge on the app icon."
         }
-        if section == 3 {
-            label.text = "The free version balabalabala....."
+        if section == 2 {
+            if versionType == .Free {
+                label.text = "The free version balabalabala....."
+            }
         }
         
         label.sizeToFit()
@@ -71,33 +87,54 @@ class SettingsViewController: BaseTableViewController, UIAlertViewDelegate, MFMa
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         tableView.deselectRowAtIndexPath(indexPath, animated: true)
         
+//        if indexPath.section == 1 {
+//            return
+//        }
         
-        if indexPath.section == 1 && indexPath.row == 0 {
-            // about
-        }
         if indexPath.section == 1 && indexPath.row == 1 {
-            // rate
-            UIApplication.sharedApplication().openURL(NSURL(string: GlobalConstants.APPSTORE_URL)!)
-        }
-        
-        if indexPath.section == 2 {
-            return
-        }
-        
-        if indexPath.section == 2 && indexPath.row == 0 {
             // go premium
+            ProductsManager.sharedInstance.purchasePro()
         }
-        if indexPath.section == 2 && indexPath.row == 1 {
+        if indexPath.section == 1 && indexPath.row == 2 {
             // restore purchase
+            ProductsManager.sharedInstance.restore()
         }
         
         if indexPath.section == 3 && indexPath.row == 0 {
             self.showSendEmailAlert()
         }
+        
+        
+        if indexPath.section == 2 && indexPath.row == 0 {
+            // about
+        }
+        if indexPath.section == 2 && indexPath.row == 1 {
+            // rate
+            UIApplication.sharedApplication().openURL(NSURL(string: GlobalConstants.APPSTORE_URL)!)
+        }
     }
     
     override func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 44.0
+        if indexPath.section == 1 && self.versionType == .Pro && (indexPath.row == 1 || indexPath.row == 2) {
+            return 0.0
+        }
+        return super.tableView(tableView, heightForRowAtIndexPath: indexPath)
+    }
+    
+    override func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+        if indexPath.section == 1 && self.versionType == .Pro && (indexPath.row == 1 || indexPath.row == 2) {
+            cell.hidden = true
+        }
+        
+        if indexPath.section == 1 && self.versionType == .Pro && indexPath.row == 0 {
+            if cell.respondsToSelector(Selector("setLayoutMargins:")) {
+                cell.layoutMargins = UIEdgeInsetsZero
+            }
+            if cell.respondsToSelector(Selector("setPreservesSuperviewLayoutMargins")) {
+                cell.preservesSuperviewLayoutMargins = false
+            }
+            cell.separatorInset = UIEdgeInsetsZero
+        }
     }
     
     // MARK: - AlertViewDelegate
@@ -145,4 +182,68 @@ class SettingsViewController: BaseTableViewController, UIAlertViewDelegate, MFMa
             cancelButtonTitle: NSLocalizedString("Yes", comment: ""))
         alert.show()
     }
+    
+    // MARK: - ProductsManagerDelegate
+    
+    func productsManager(productsManager: ProductsManager, paymentTransactionState state: SKPaymentTransactionState) {
+        switch state {
+        case SKPaymentTransactionState.Purchased:
+            println("Purchased")
+            break
+            
+        case SKPaymentTransactionState.Restored:
+            println("Restored")
+            break
+            
+        case SKPaymentTransactionState.Failed:
+            println("Failed")
+            break
+            
+        default:
+            break
+        }
+    }
+    
+    func refreshThePaymentItem(versionType: VersionType) {
+        switch versionType {
+        case .Free:
+            self.currentVersionLabel.text = "Free"
+            break
+            
+        case .Pro:
+            self.currentVersionLabel.text = "Pro"
+            break
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
